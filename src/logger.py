2d61cea7a1359a1cec4aded2df72053065392bdd
AsiehH <asieh.harati.w@gmail.com>
2023-04-28 16:25:57 -0400

    fixed if log dir does not exist

--- src/logger.py
+++ src/logger.py
@@ -4,6 +4,7 @@ from datetime import datetime
 
 LOG_FILE = f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}.log"
 logs_path = os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_path, exist_ok=True)
 
 LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
